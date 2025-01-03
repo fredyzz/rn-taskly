198bd763883e829f6c642af2fa4994705e2d3c82
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2025-01-03 13:24:25 +0100

    Add navigation, test stack and modal navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Shopping list" }} />
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
+    </Stack>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
 import { StyleSheet, View } from "react-native";
 
-import { theme } from "./theme";
-import { ShoppingListItem } from "./components/ShoppingListItem";
+import { theme } from "../theme";
+import { ShoppingListItem } from "../components/ShoppingListItem";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href="/counter"
+        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
+      >
+        Go to counter
+      </Link>
       <ShoppingListItem name="Coffe" />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Sugar" />
