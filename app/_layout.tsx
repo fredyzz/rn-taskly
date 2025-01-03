7b537327feb2082a996b63f4f9861a730f590d8e
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2025-01-03 13:49:57 +0100

    Combina tab navigation with stack navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,38 @@
 import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import Entypo from "@expo/vector-icons/Entypo";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Shopping list" }} />
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulen }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping list",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="counter"
         options={{
           title: "Counter",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="idea"
         options={{
           title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="light-bulb" size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
